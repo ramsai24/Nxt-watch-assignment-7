d9834fe9525d01060419b567da03ebd24fcc523b
jnrsai24 <jnrsai24@gmail.com>
2023-09-13 09:55:47 +0000

    false value is not rendering in web page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Login from './components/Login'
 import './App.css'
 
 // Replace your code here
-const App = () => <div>Hello World</div>
+const App = () => (
+  <div>
+    <Login />
+  </div>
+)
 
 export default App
